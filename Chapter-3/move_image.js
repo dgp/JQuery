a5a408589c9c064993e068af8ef5e070b74baf22
M. Dinesh Gowtham Prathap <dinesh@activesphere.com>
2011-09-27 19:17:44 +0530

    hide and show

--- Chapter-3/move_image.js
+++ Chapter-3/move_image.js
@@ -1,10 +1,28 @@
 $(function() {
   $('.bottom_button').click(function() {
     $('.bottom_content img:first').appendTo('.top_content');
-    $('this').addClass('hide');
+    var tag1 = $('.bottom_content').find('img')
+    if (tag1.length == 0) 
+    {
+      $('.bottom_button').addClass('hide_button');
+    }
+    var tag2 = $('.top_content').find('img')
+    if (tag2.length >= 0)
+    {
+      $('.top_button').removeClass('hide_button');
+    }
   });
   $('.top_button').click(function() {
     $('.top_content img:first').appendTo('.bottom_content');
+    var tag3 = $('.top_content').find('img')
+    if (tag3.length == 0)
+    {
+      $('.top_button').addClass('hide_button');
+    }
+    var tag4 = $('.bottom_content').find('img')
+    if (tag4.length >= 0)
+    {
+      $('.bottom_button').removeClass('hide_button');
+    }
   });
-  
 });
