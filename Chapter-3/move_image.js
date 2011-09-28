2e18928e46884fc49457de30b0880950e13854af
M. Dinesh Gowtham Prathap <dinesh@activesphere.com>
2011-09-28 11:26:51 +0530

    changes in nameing convention

--- Chapter-3/move_image.js
+++ Chapter-3/move_image.js
@@ -1,27 +1,26 @@
-$(function() {
-  $('.bottom_button').click(function() {
+$(function () {
+  $('.top_button').addClass('hide_button');
+  $('.bottom_button').click( function () {
     $('.bottom_content img:first').appendTo('.top_content');
-    var tag1 = $('.bottom_content').find('img')
-    if (tag1.length == 0) 
-    {
+    var length = 0;
+    length = $('.bottom_content').find('img').length
+    if (length === 0) {
       $('.bottom_button').addClass('hide_button');
     }
-    var tag2 = $('.top_content').find('img')
-    if (tag2.length >= 0)
-    {
+    length = $('.top_content').find('img').length
+    if (length >= 0) {
       $('.top_button').removeClass('hide_button');
     }
   });
   $('.top_button').click(function() {
     $('.top_content img:first').appendTo('.bottom_content');
-    var tag3 = $('.top_content').find('img')
-    if (tag3.length == 0)
-    {
+    var length;
+    length = $('.top_content').find('img').length
+    if (length == 0) {
       $('.top_button').addClass('hide_button');
     }
-    var tag4 = $('.bottom_content').find('img')
-    if (tag4.length >= 0)
-    {
+    length = $('.bottom_content').find('img').length
+    if (length >= 0) {
       $('.bottom_button').removeClass('hide_button');
     }
   });
